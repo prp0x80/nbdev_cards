ad96c0654acba5d0989d8f2f3e977a52abe72d0a
Prashant Patel <prashant.patel@kainos.com>
2022-09-30 19:44:37 +0000

    Added card and test

--- nbdev_cards/_modidx.py
+++ nbdev_cards/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://prp0x80.github.io',
                 'git_url': 'https://github.com/prp0x80/nbdev_cards',
                 'lib_path': 'nbdev_cards'},
-  'syms': {'nbdev_cards.core': {'nbdev_cards.core.foo': ('core.html#foo', 'nbdev_cards/core.py')}}}
+  'syms': { 'nbdev_cards.card': { 'nbdev_cards.card.Card': ('card.html#card', 'nbdev_cards/card.py'),
+                                  'nbdev_cards.card.Card.__init__': ('card.html#card.__init__', 'nbdev_cards/card.py'),
+                                  'nbdev_cards.card.Card.__str__': ('card.html#card.__str__', 'nbdev_cards/card.py')}}}
